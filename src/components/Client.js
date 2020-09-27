731f1f29eba4778f645b29a8bac9c8de2c737c09
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-09-27 00:04:31 -0400

    added config

--- src/components/Client.js
+++ src/components/Client.js
@@ -5,7 +5,8 @@ import { setContext } from '@apollo/client/link/context';
 
 
 const httpLink = new createHttpLink({ uri: 'https://api.github.com/graphql' });
-const token = '';
+const token = process.env.REACT_APP_TOKEN;
+console.log(token);
 const graphQLClient = (props) => {
 
   const authLink = setContext((_, { headers }) => {
