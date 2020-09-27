f26a74485b83f9e1ed45acbde282f2484981e307
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-27 22:21:57 +0000

    Deploy chinmay60/chinmay60 to github.com/chinmay60/chinmay60.git:gh-pages

---
+++ precache-manifest.c6b01f5284c846b15b25b428933bc6db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c71853430483a689c9d691467186886b",
+    "url": "/chinmay60/index.html"
+  },
+  {
+    "revision": "29fe142ad79b181817fb",
+    "url": "/chinmay60/static/css/main.9ce9eb75.chunk.css"
+  },
+  {
+    "revision": "dab3513a94ae97e3d94d",
+    "url": "/chinmay60/static/js/2.a10169d8.chunk.js"
+  },
+  {
+    "revision": "7d763b78697d0d0900e3a3ce04ca0d45",
+    "url": "/chinmay60/static/js/2.a10169d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29fe142ad79b181817fb",
+    "url": "/chinmay60/static/js/main.1798e2cc.chunk.js"
+  },
+  {
+    "revision": "a7ca8af9a55c8e6cb8a8",
+    "url": "/chinmay60/static/js/runtime-main.ada6100f.js"
+  }
+]);
