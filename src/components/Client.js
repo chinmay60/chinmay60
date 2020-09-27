dbc292781f0cb14183325b6d1ec9653e2aeb215c
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-09-27 15:28:44 -0400

    add travis CI support

--- src/components/Client.js
+++ src/components/Client.js
@@ -5,7 +5,7 @@ import { setContext } from '@apollo/client/link/context';
 
 
 const httpLink = new createHttpLink({ uri: 'https://api.github.com/graphql' });
-const token = process.env.REACT_APP_GITHUB;
+const token = process.env.REACT_APP_GITHUB_TOKEN;
 console.log(token);
 const graphQLClient = (props) => {
 
