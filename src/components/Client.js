c9f5d6eca7513117872b37438a6f0e45a20d670a
Chinmay Vinchurkar <chinmayvinchurkar24@gmail.com>
2020-11-01 23:25:53 -0500

    add token as env vaiable

--- src/components/Client.js
+++ src/components/Client.js
@@ -4,7 +4,7 @@ import { ApolloProvider } from "react-apollo";
 import { setContext } from "@apollo/client/link/context";
 
 const httpLink = new createHttpLink({ uri: "https://api.github.com/graphql" });
-const token = "656ec9af616d919078fa266222f983b8367f606e";
+const token = process.env.REACT_APP_GITHUB_TOKEN;
 
 const graphQLClient = (props) => {
   const authLink = setContext((_, { headers }) => {
